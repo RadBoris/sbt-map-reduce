c8fea52e4f1f123f592a967853c800843d5ed003
RadBoris <rad.borislavov@gmail.com>
2019-06-11 17:33:51 +0000

    first

---
+++ src/main/scala/org/radboris/sbt/Map.scala
@@ -0,0 +1,6 @@
+package org.radboris.sbt
+
+object Map  {
+  // override def trigger = allRequirements
+  // override def requires = JvmPlugin
+}
---
+++ src/test/scala/org/radboris/sbt/MapReduceSpec.scala
@@ -0,0 +1,5 @@
+package org.radboris.sbt
+
+class MapReduceTest {
+  // write tests with your preferred framework
+}
